9995b6396eb13340d22224784042fcda91fbf7d9
kmyqchy <kmyqchy@gmail.com>
2018-08-13 15:37:42 +0800

    ‘’

--- ttest.py
+++ ttest.py
@@ -1,5 +1,40 @@
 #Filename:ttest.py
+# coding=utf-8
 
-import sys
-import numpy as np
+from urllib import request,parse
+#import re
+#import http
+
+'''
+a='adfwqepzz,msfkwew123dfws35%^*'
+print(a)
+print(re.match('www', 'www.runoob.com').span()) 
+b=re.search('sf',a).span()
+print(b)
+'''
+class conrequest:
+    def __init__(self,url):
+        self.url=url       
+    #这是注释
+    def con(self):
+        headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0;WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.221 Safari/537.36 SE 2.X MetaSr 1.0'}
+        
+        #进行请求，吧request对象传入urlopen参数中
+        requests = request.Request(self.url,headers=headers)
+        response = request.urlopen(requests)
+        print(response.read().decode('utf-8'))
+        
+        print(response.status)
+        
+        print(response.getheaders())
+        
+        for k,v in response.getheaders():
+            print(k,'=',v)
+
+'''X=conrequest('https://www.google.com')
+X.con()
+'''
+
+
+
 
